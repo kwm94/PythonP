10bb275f787a618cc089b3a81e142146a0beea6c
Kit Wei Min <kit.weimin@dhs.sg>
2011-02-23 10:25:49 +0800

    added the completed find_prime.py

--- find_prime.py
+++ find_prime.py
@@ -2,17 +2,37 @@
 # find the nth prime number efficiently
 
 
-def is_prime(k):
 ''' Function to determine if a number is prime
     Input: integer k
     Output: true if k is prime, else false
-''' 
+'''
+import math
+def is_prime(k):
+    if k == 2:
+        return True
+    elif k % 2 == 0:
+        return False
+    else:
+        temp = int(math.sqrt(k))+1
+        for i in range(3, temp, 2):
+            if k % i == 0:
+                return False
+    return True
 
 # main
 # get input n
 n = int(input("Enter n: "))
 
 # find nth prime number
+count = 1 # number of primes so far
+num = 1 # starting number to test for primality
+if n == 1:
+    num = 2
+else:
+    while count < n:
+        num = num + 2
+        if is_prime(num):
+            count = count + 1
 
 # display
-print(ans)
+print(num)
